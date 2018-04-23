da2193cc8a4cd51fddfbfabf2002a55dca2dcc83
Sudharshan <sudhar393@gmail.com>
2018-04-23 19:22:50 +0800

    added simple script for setup

---
+++ setup-env.bash
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# High simplified write a proper script
+docker start samba && sudo ifconfig lo0 127.0.0.2 alias up
+docker run --rm -it -v poky:/workdir crops/poky --workdir=/workdir
