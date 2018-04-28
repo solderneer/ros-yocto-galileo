1afaf970e4a96a8164c611c210de9ee8a5c2636a
Sudharshan <sudhar393@gmail.com>
2018-04-28 22:10:04 +0800

    updated bash

--- setup-env.bash
+++ setup-env.bash
@@ -2,4 +2,4 @@
 
 # High simplified write a proper script
 docker start samba && sudo ifconfig lo0 127.0.0.2 alias up
-docker run --rm -it -v poky:/workdir crops/poky --workdir=/workdir
+docker run --rm -it -v poky:/workdir solderneer/galileo-build --workdir=/workdir
